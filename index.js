516a0056f5b402e64c28133b1749b979ff86580d
Indya 2 <indya2@Steves-MacBook-Pro.local>
2022-01-06 12:36:04 +0000

    array tasks finished

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
 // Arrays:
 
 // Immutably add an item to the end of an array
-export function addToEnd(array, item) {}
+export function addToEnd(array, item) {
+  const newArray = [...array, item];
+  return newArray;
+}
 
 // Immutably add an item to the beginning of an array
-export function addToBeginning(array, item) {}
+export function addToBeginning(array, item) {
+  const newArray = [item, ...array];
+  return newArray;
+}
 
 // Immutably insert an item at a specific position/index within an array
-export function insertItem(array, item, index) {}
+export function insertItem(array, item, index) {
+  const newArray = [...array.slice(0, index), item, ...array.slice(index)];
+  return newArray;
+}
 
 // Immutably replace an item at a specific position/index within an array
-export function replaceItem(array, item, index) {}
+export function replaceItem(array, item, index) {
+  // const newArray = [...array.splice(index, 0, item)]
+  const newArray = [...array.slice(0, index), item, ...array.slice(index + 1)];
+  return newArray;
+}
 
 // Immutably remove an item at a specific position/index within an array
-export function removeItem(array, index) {}
+export function removeItem(array, index) {
+  const newArray = [...array.slice(0, index), ...array.slice(index + 1)];
+  return newArray;
+}
 
 //Objects:
 
@@ -22,7 +38,9 @@ export function removeItem(array, index) {}
 //     updateName({ name: "Abe" }, "Barbara")
 // should give back:
 //     { name: "Barbara"}
-export function updateName(object, newName) {}
+export function updateName(object, newName) {
+  const newObject = { ...object, object.name: newName };
+}
 
 // Immutably update the object so that the value under the "needsACupOfTea" property becomes the opposite of what it was.
 // Any other properties in the object should be maintained.
